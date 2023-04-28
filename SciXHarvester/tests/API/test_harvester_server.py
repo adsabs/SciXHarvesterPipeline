06a41c36e856dff505efc19562d27329e28967c1
tjacovich <tjacovich@cfa.harvard.edu>
2023-04-28 11:04:38 -0400

    Initial sketch of server testing.

--- SciXHarvester/tests/API/test_harvester_server.py
+++ SciXHarvester/tests/API/test_harvester_server.py
@@ -1,40 +1,32 @@
-# import pytest
-# import avro
-# from unittest import TestCase
-# from harvester_gRPC.gRPCHarvester.avro_serializer import AvroSerialHelper
-# import harvester_gRPC.gRPCHarvester.grpc_modules.harvester_grpc as harvester_grpc
+from concurrent import futures
+from unittest import TestCase
 
-# @pytest.fixture(scope='module', params=[AvroSerialHelper(open("AVRO_schemas/HarvesterInputSchema.avsc", "r").read())])
-# def grpc_server(_grpc_server, grpc_addr, grpc_add_to_server, grpc_servicer, request):
-#     grpc_add_to_server(grpc_servicer, _grpc_server, request.param)
-#     _grpc_server.add_insecure_port(grpc_addr)
-#     _grpc_server.start()
-#     yield _grpc_server
-#     _grpc_server.stop(grace=None)
+import grpc
 
-# @pytest.fixture(scope='module', params=[AvroSerialHelper(open("AVRO_schemas/HarvesterInputSchema.avsc", "r").read())])
-# def grpc_stub(grpc_stub_cls, grpc_channel, request):
-#     return grpc_stub_cls(grpc_channel, request.param)
+from API.grpc_modules import harvester_grpc
 
-# @pytest.fixture(scope='module')
-# def grpc_add_to_server(request):
-#     from harvester_gRPC.gRPCHarvester.grpc_modules.harvester_grpc import add_HarvesterInitServicer_to_server
-#     return add_HarvesterInitServicer_to_server
 
-# @pytest.fixture(scope='module')
-# def grpc_servicer():
-#     from harvester_gRPC.gRPCHarvester.grpc_modules.harvester_grpc import HarvesterInitServicer
+def HarvesterServer(cls):
+    """Instantiate a harvester server and return a stub for use in tests"""
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
+    harvester_grpc.add_HarvesterInitServicer_to_server(cls(), server)
+    port = server.add_insecure_port("[::]:0")
+    server.start()
 
-#     return HarvesterInitServicer()
+    try:
+        with grpc.insecure_channel("localhost:%d" % port) as channel:
+            yield harvester_grpc.HarvesterInitStub(channel)
+    finally:
+        server.stop(None)
 
-# @pytest.fixture(scope='module')
-# def grpc_stub_cls(grpc_channel):
-#     from harvester_gRPC.gRPCHarvester.grpc_modules.harvester_grpc import HarvesterInitStub
-#     return HarvesterInitStub
 
-# @pytest.mark.usefixtures("grpc_add_to_server", "grpc_servicer", "grpc_stub_cls")
-# def test_server_initialization(grpc_stub):
-#     request = harvester_grpc.HarvesterInit()
-#     response = grpc_stub.initHarvester(request)
+class TestHarvesterServer(TestCase):
+    def test_Harvester_server(self):
+        # may do something extra for this mock if it's stateful
+        class FakeHarvesterServer(harvester_grpc.HarvesterInitServicer):
+            def initHarvester(self, request, context):
+                return request
 
-#     assert response.name == f'test-{request.name}'
+        # with HarvesterServer(FakeHarvesterServer) as stub:
+        #     response = stub.SayHello(helloworld_pb2.HelloRequest(name='Jack'))
+        #     self.assertEqual(response.message, 'Hello, Jack!')
